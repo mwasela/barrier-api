4ddbff5afb887e538d8d309d1903a1eeb25a297d
mwasela <dennysmycall@gmail.com>
2024-03-26 11:02:19 +0300

    new file:   .env 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ const client = redis.createClient();
 var BarrierLogs = require('../models/BarrierLogs');
 var User = require('../models/User');
 const authenticateToken = require("../middlewares/auth");
+const { default: axios } = require('axios');
+require('dotenv').config();
+//API_URL='http://192.168.0.151/relay?state=on'
+API_URL='http://localhost:3000/relay?state=on'
 // const app = express();
 
 
@@ -18,10 +22,10 @@ client.on('error', function (err) {
 let lastPlateNumber = '';
 let carDump = [
   {
-  'plateNumber': 'plateNumber_01',
-  'plateColor': 'plateColor_01',
-  'deviceID': 'deviceID_01',
-  'direction': 'direction_01',
+    'plateNumber': 'plateNumber_01',
+    'plateColor': 'plateColor_01',
+    'deviceID': 'deviceID_01',
+    'direction': 'direction_01',
   },
   {
     'plateNumber': 'plateNumber_02',
@@ -29,10 +33,33 @@ let carDump = [
     'deviceID': 'deviceID_02',
     'direction': 'direction_02',
   }
-  ];
+];
+
+
+const BarrierTrigger = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.status === 200 ? 200 : 500;
+  } catch (err) {
+    console.error(err);
+    return 500;
+  }
+}
+
+router.get('/relay', async function (req, res){
+  const state = req.query.state;
+
+  if (state === 'on') {
+    //relay('on');
+    return res.status(200).send("Relay turned on successfully.");
+  } else {
+    return res.status(500).send("Invalid state provided.");
+  }
+});
+
 
 /* GET home page. */
-router.get('/plateInfo',authenticateToken, function (req, res) {
+router.get('/plateInfo', authenticateToken, function (req, res) {
   // Retrieve data from Redis
   const plate = carDump;
   return res.status(200).json(plate);
@@ -40,24 +67,54 @@ router.get('/plateInfo',authenticateToken, function (req, res) {
 });
 
 
-
-
 router.post('/barrierLogs', authenticateToken, async function (req, res, next) {
   const data = req.body;
+  const theDirection = data.direction;
+  const loggedIN = req.user.id;
+
   try {
-    const loG = BarrierLogs.create({
-      plateNumber: data.plateNumber,
-      plateColor: data.plateColor,
-      deviceID: data.deviceID,
-      direction: data.theDirection,
-      createdby: data.user,
+    // Find in carDump the record whose carDump.PlateNumber == data.plateNumber.
+    //const carRecord = carDump.find(car => car.PlateNumber === data.plateNumber);
+    const carRecord = carDump.find(car => car.plateNumber === data.plateNumber);
+
+    if (!carRecord) {
+      return res.status(404).json({ message: "Car record not found." });
+    }
+
+    const loG = await BarrierLogs.create({
+      plateNumber: carRecord.plateNumber,
+      plateColor: carRecord.plateColor,
+      deviceID: carRecord.deviceID,
+      direction: theDirection,
+      createdby: loggedIN,
     });
-    return res.status(200).json({ status: 200, loG });
+
+    const request = BarrierTrigger();
   } catch (err) {
     return res.status(500).json({ message: err.message });
   }
-}
-);
+});
+
+// router.post('/barrierLogs', authenticateToken, async function (req, res, next) {
+//   const data = req.body;
+//   const theDirection = data.direction;
+//   //find in carDump the record whose carDump.PlateNumber == data.plateNumber.
+//   //Use the record details to feed barrierLogs.Create below.
+
+//   try {
+//     const loG = BarrierLogs.create({
+//       plateNumber: data.plateNumber,
+//       plateColor: data.plateColor,
+//       deviceID: data.deviceID,
+//       direction: theDirection,
+//       createdby: data.user,
+//     });
+//     return res.status(200).json({ status: 200, loG });
+//   } catch (err) {
+//     return res.status(500).json({ message: err.message });
+//   }
+// }
+// );
 
 //get logs
 
@@ -83,7 +140,7 @@ router.get('/barrierLogs', authenticateToken, async function (req, res, next) {
 
 
 
-router.post('/NotificationInfo/KeepAlive', authenticateToken, async function (req, res, next) {
+router.post('/NotificationInfo/KeepAlive', async function (req, res, next) {
   const data = req.body;
   console.log("req data", data);
   return res.status(200).json("ok");
@@ -98,25 +155,25 @@ router.put('/plate', authenticateToken, async (req, res) => {
   console.log(req.body);
 
   try {
-      const unit = await BarrierLogs.update({
+    const unit = await BarrierLogs.update({
 
-          plateNumber: _plate
-          
-      }, {
-          where: {
-              id: _id
-          }
-      });
+      plateNumber: _plate
 
-      return res.status(201).json({ message: 'Record updated successfully' });
+    }, {
+      where: {
+        id: _id
+      }
+    });
+
+    return res.status(201).json({ message: 'Record updated successfully' });
   } catch (err) {
-     return  res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
 }
 );
 
 
-router.post('/NotificationInfo/TollgateInfo', authenticateToken, async function (req, res, next) {
+router.post('/NotificationInfo/TollgateInfo', async function (req, res, next) {
   const data = req.body;
   //console.log("req data", data);
   const plateNumber = data.Picture.Plate.PlateNumber;
@@ -126,7 +183,7 @@ router.post('/NotificationInfo/TollgateInfo', authenticateToken, async function 
 
   if (plateNumber !== lastPlateNumber) {
     // Store data in carDump array
-    
+
     carDump.push({
       'plateNumber': plateNumber,
       'plateColor': plateColor,
